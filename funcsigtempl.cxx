2531591261fcee08a797dbdfbd606533e79d1981
lukasheinrich <lukas@pb-d-128-141-230-212.cern.ch>
2014-07-27 02:48:21 +0200

    factory

--- funcsigtempl.cxx
+++ funcsigtempl.cxx
@@ -6,16 +6,34 @@ up when I looked into the SEAL Reflex PluginService
 
 */
 
+//this just declares the class
 template<typename T>
-struct factory_with_sig{
-	T* member;
+struct factory;
+
+//now we specialice so we get access to the ctor types
+template<typename R>
+struct factory<R (void)>{
+	R (*member)(void);
+};
+
+template<typename R,typename A1>
+struct factory<R (A1)>{
+	R (*member)(void);
+};
+
+template<typename T> struct pointer2type;
+template<typename T> struct pointer2type<T*>{typedef T type;};
+
+template<typename R,typename A1, typename A2>
+struct factory<R (A1,A2)>{
+	R (*member)(A1,A2);
+	static R create(A1 a1,A2 a2){return new typename pointer2type<R>::type(a1,a2);}
 };
 
 
 
 struct object{
 	object(int i,double d){m_int = i;m_double = d;}
-	static object* create(int i,double d){return new object(i,d);} //factory method
 	int m_int;
 	double m_double;
 };
@@ -27,11 +45,11 @@ void func(int i,double d){
 }
 
 int main(){
-	factory_with_sig<object* (int, double)> f;
-	f.member = &object::create;
+	factory<object* (int, double)> f;
 
+	f.member = &factory<object* (int, double)>::create;
 
-	object* o = f.member(2,3.14);
+	object* o = f.create(2,3.14);
 	
 	std::cout << o->m_int << std::endl;
 	std::cout << o->m_double << std::endl;
