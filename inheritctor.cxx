5be12d2c3438decbef53787fa9c50d8a3851fb29
lukasheinrich <lukas@pb-d-128-141-152-15.cern.ch>
2014-07-29 17:07:42 +0200

    inheriting ctors with c++11

--- inheritctor.cxx
+++ inheritctor.cxx
@@ -1,3 +1,20 @@
+/*
+	just a small demonstration of the new C++ feature of automatically inheriting all ctors
+	from a base class. The use case is that if Base has many ctors whose signature you want
+	to duplicate in the derived class (and just let that use the base ctor) you can just use
+	the declaration using Base::Base
+	compile using: g++ -o inheritctor inheritctor.cxx --std=c++11
+	expected output is:
+	>int
+	>my own int 
+	>const char
+	>double
+	
+	if you want to override one of the signatures (e.g. do some additional stuff in the derived initialization)
+	you can just define such a ctor and the overloading will select the derived ctor first. (here: the int ctor)
+*/
+
+
 #include <iostream>
 struct Parent{
 	Parent(int i){std::cout << "int" << std::endl;;}
