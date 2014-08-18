92ed03d6d4449de98c431d4cbf9cd3cbe02457c4
Lukas Heinrich <lukas.heinrich@gmail.com>
2014-08-18 22:20:24 +0200

    cleanup

--- getamitags.py
+++ getamitags.py
@@ -5,7 +5,7 @@ import IPython
 
 dataset = 'mc12_8TeV.105200.McAtNloJimmy_CT10_ttbar_LeptonFilter.merge.NTUP_SUSY.e1513_s1499_s1504_r3945_r3549_p1328/'
 ami = pyAMI.client.AMI()
-provenance = pyAMI.query.get_provenance(ami,dataset)
+#provenance = pyAMI.query.get_provenance(ami,dataset) #just for fun but slow
 
 leafversion = pyAMI.query.get_dataset_info(ami,dataset).info['version']
 amitags = pyAMI.query.get_configtags(ami,leafversion)
