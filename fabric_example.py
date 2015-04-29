d91af08d826459177d0ff3905e5b605fee5458c8
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-04-29 14:15:41 -0400

    cleanup

--- fabric_example.py
+++ fabric_example.py
@@ -1,6 +1,6 @@
-import fabric
 from fabric.api import env
 from fabric.operations import run, put
+from fabric.tasks import execute
 
 env.hosts = ['nyu']
 env.use_ssh_config = True
@@ -9,5 +9,4 @@ def copy():
     run('mkdir -p ~/fromfabric')
     put('./zenodo_testfile.txt','~/fromfabric')
     
-from fabric.tasks import execute
 execute(copy,hosts = ['nyu'])
