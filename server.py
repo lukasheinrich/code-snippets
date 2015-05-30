8387127663fe1849aaae7db7260d49ce10ee8056
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-05-30 13:25:38 -0400

    super simple nbconvert webservice

--- server.py
+++ server.py
@@ -1,16 +1,10 @@
+import subprocess
 from flask import Flask, request
 app = Flask(__name__)
-
-import subprocess
-
 @app.route('/')
 def render():
   path = request.args.get('notebook', '')
-
-  print path
-
   out,err = subprocess.Popen(['ipython','nbconvert','--to','html',path,'--stdout'],stdout=subprocess.PIPE).communicate()
   return out
-
 if __name__ == '__main__':
     app.run()
