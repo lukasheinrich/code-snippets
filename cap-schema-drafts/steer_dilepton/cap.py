0c6edcf13439d9c50e6032d64a7b0d6e3c63ad47
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-01-21 17:31:06 -0500

    no loading

--- cap-schema-drafts/steer_dilepton/cap.py
+++
@@ -1,4 +0,0 @@
-import json
-def load_spec(spec):
-  with open('capdata/{}.json'.format(spec)) as f:
-    return json.load(f)
--- cap-schema-drafts/steer_dilepton/convert.py
+++
@@ -1,8 +0,0 @@
-import json
-import yaml
-import glob
-
-for x in  glob.glob("*.yaml"):
-  with open(x) as f:
-    with open(x.replace('.yaml','.json'),'w') as out:
-      json.dump(yaml.load(f),out,sort_keys=True,indent=2, separators=(',', ': '))
