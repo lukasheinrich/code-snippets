d704fac755246961439e155a26e7a03730e9c151
lukasheinrich <lukas.heinrich@gmail.com>
2016-09-08 14:08:56 +0200

    added interactivity

--- docker-buildhelpers/buildscripts/build.sh
+++ docker-buildhelpers/buildscripts/build.sh
@@ -4,3 +4,13 @@ mkdir -p /code/package
 echo "hello i am a fragment new" > /code/package/frag.txt
 echo "done building"
 cp /.dockerbuild/resources/resource.txt /code/package
+
+while true; do
+    read -p "Do you wish to install this program?" yn
+    case $yn in
+        [Yy]* ) echo "great!"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
