f008bb36e957135405f726aa0ae4b30e28141df1
lukasheinrich <lukas.heinrich@gmail.com>
2017-05-05 15:30:17 +0200

    dlopen etc

---
+++ factory/README.md
@@ -0,0 +1,3 @@
+simple factory pattern / plugin setup with plugins in separate shared libraries
+
+    make lib1 lib2 main
--- factory/main.cxx
+++ factory/main.cxx
@@ -1,6 +1,8 @@
 #include "interface.h"
 #include "factory.h"
 
+#include <dlfcn.h>
+
 //as we can see we are only including the interface and the factory headers
 //we don't need to include any headers for the concrete implementation
 //it is enough that those are linked against by the compiler
@@ -14,6 +16,8 @@
 //what would now be the next step would be to have a manager for arbitrary signatures
 
 int main(){
+	dlopen("./libimpl1.so", RTLD_NOW);
+	dlopen("./libimpl2.so", RTLD_NOW);
 	auto m = interfacefactory<interface>::instance();
 	for(auto i : {"impl1","impl2","impl3"}){
 		auto impl = m.create(i);
